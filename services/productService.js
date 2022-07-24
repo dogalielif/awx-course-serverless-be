6a0b633c97d18899a59e6e1630a42e37d83b99ff
Elif Dogali <dogali.elif@gmail.com>
2022-07-24 16:46:31 +0300

    Remove environment variables

--- services/productService.js
+++ services/productService.js
@@ -2,7 +2,7 @@ import db from "../db";
 
 export const getDBProducts = async () => {
   try {
-    const products = await db.query('SELECT * FROM products')
+    const products = await db.query('SELECT * FROM products as p inner join stocks as s on p.id = s.product_id')
     return products?.rows;
   } catch (e) {
     throw new Error(e);
