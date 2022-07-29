106b53fe2f20d16ebdbc334dee303bb9dc77f2b9
Elif Dogali <dogali.elif@gmail.com>
2022-07-29 22:32:09 +0300

    Rename product service

---
+++ product-service/services/productService.js
@@ -0,0 +1,33 @@
+import db from "../db";
+
+export const getDBProducts = async () => {
+  const resp = await db.query('SELECT * FROM products as p inner join stocks as s on p.id = s.product_id')
+  return resp?.rows ? resp?.rows : resp;
+}
+
+export const getDBProductById = async (id) => {
+    const resp = await db.query('SELECT * FROM products WHERE id = $1', [id])
+    return resp?.rows ? resp?.rows : resp;
+}
+
+export const createDBProduct = async (product) => {
+  if(!product) {
+    const error = new Error('Product cannot be null')
+    error.statusCode = 400;
+    throw error; 
+  }
+ 
+  const { title, price, description } = product;
+  if(!title || !price || !description) {
+    const error = new Error('Product should have title, price and description properties');
+    error.statusCode = 400;
+    throw error; 
+  }
+  const resp = await db.query('INSERT INTO products(title, description, price) VALUES($1, $2, $3) RETURNING *', [title, description, price])
+  if(resp?.rows?.[0]) {
+    await db.query('INSERT INTO stocks(product_id, count) VALUES($1, $2) RETURNING *', [resp?.rows[0].id, 3])
+  }
+  return resp?.rows ? resp.rows : resp;
+ 
+}
+
