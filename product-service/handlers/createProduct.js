1e5ffe2f7c655af605fe51e01798110564e59748
Elif Dogali <dogali.elif@gmail.com>
2022-07-29 22:38:17 +0300

    Change paths to imports

--- product-service/handlers/createProduct.js
+++ product-service/handlers/createProduct.js
@@ -1,5 +1,5 @@
 import { createDBProduct } from '../services/productService';
-import { requestHandler } from '../utils/http.util';
+import { requestHandler } from '../../utils/http.util';
 
 export const createProduct = (event) => requestHandler(event, async (event) => {
   const product = event?.body && JSON.parse(event.body);
